8d93a1d4659cd54965f40e46630662e8460a92a5
Mubsshir <39831189+Mubsshir@users.noreply.github.com>
2021-04-11 00:18:20 +0530

    Add files via upload

---
+++ ufo.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include "ufo.hpp"
+
+int main(){
+    great();
+    std::string codeword="codecademy";
+    std::string answer="__________";
+    int misses=0;
+    std::vector<char> incorrect{};
+    bool guess= false;
+
+    char letter;
+    while(answer!=codeword && misses<7){
+        display_misses(misses);
+        std::cout << "Please Enter Your Guess: ";
+        std::cin >> letter;
+        for (int i=0;i<codeword.size();i++){
+            if(letter==codeword[i]){
+                answer[i]=letter;
+                guess=true;
+            }
+        }
+        if (guess)
+        {
+            std::cout << "\nCorrect!";
+        }
+        else{
+            std::cout << "\nIncorrect! The tractor veam pulls the person in further.";
+            incorrect.push_back(letter);
+            misses++;
+        }
+        
+        display_status(incorrect,answer);
+        
+
+    }
+    end_game(answer,codeword);
+
+    
+    
+}

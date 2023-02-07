cfa6e7e76a78fcc044d9159706fd11199aa7a928
Mubasshir Khan <39831189+Mubsshir@users.noreply.github.com>
2023-02-07 23:05:59 +0530

    Update ufo.hpp

--- ufo.hpp
+++ ufo.hpp
@@ -3,7 +3,7 @@
 void display_misses(int misses);
 
 // Declare functions
-void great();
+void greet();
 void end_game(std::string answer,std::string codeword);
 void display_status(std::vector<char> incorrect,std::string answer);
 void game();
